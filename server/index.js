61ee5deba0ac5db733367976f2dfedc9e758379a
Andrew Leung <leung.andrew@gmail.com>
2018-07-22 15:41:03 -0400

    Update package.json and add manifest for deploying to CF

--- server/index.js
+++ server/index.js
@@ -46,6 +46,6 @@ const server = new ApolloServer({ typeDefs, resolvers });
 
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
-server.listen().then(({ url }) => {
+server.listen(process.env.PORT || 3000).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
