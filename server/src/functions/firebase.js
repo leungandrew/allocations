91b49ad6ea1ca8227fa7f7b90de15c5022293a9b
Andrew Leung <leung.andrew@gmail.com>
2018-08-13 10:57:05 -0400

    Import firebase-functions correctly

--- server/src/functions/firebase.js
+++ server/src/functions/firebase.js
@@ -1,9 +1,10 @@
 import * as firebase from 'firebase';
+import * as functions from 'firebase-functions';
 
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID
+  apiKey: functions.config().fbconfig.api_key || process.env.FIREBASE_API_KEY,
+  authDomain: functions.config().fbconfig.auth_domain || process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: functions.config().fbconfig.project_id || process.env.FIREBASE_PROJECT_ID
 };
 
 export default !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
